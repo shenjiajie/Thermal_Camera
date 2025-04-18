c8dd94e3e96dce50ad38917dfa5fda9c2fc38d5b
sJJay <1972133579@qq.com>
2025-04-18 21:36:10 +0800

    完成class-colormap，实现正常的热成像画面

---
+++ Heimann_Cam/app.d
@@ -0,0 +1,101 @@
+app: sdl2_draw.c sdl2_draw.h /opt/sdl2-rk3588-install/include/SDL2/SDL.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_main.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_stdinc.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_config.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_platform.h \
+ /opt/sdl2-rk3588-install/include/SDL2/begin_code.h \
+ /opt/sdl2-rk3588-install/include/SDL2/close_code.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_assert.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_atomic.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_audio.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_error.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_endian.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_mutex.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_thread.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_rwops.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_clipboard.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_cpuinfo.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_events.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_video.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_pixels.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_rect.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_surface.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_blendmode.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_keyboard.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_keycode.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_scancode.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_mouse.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_joystick.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_gamecontroller.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_sensor.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_quit.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_gesture.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_touch.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_filesystem.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_haptic.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_hints.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_loadso.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_log.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_messagebox.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_metal.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_power.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_render.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_shape.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_system.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_timer.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_version.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_locale.h \
+ /opt/sdl2-rk3588-install/include/SDL2/SDL_misc.h heimann_inter.h \
+ heimann_drv.h heimann_reg.h
+sdl2_draw.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_main.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_stdinc.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_config.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_platform.h:
+/opt/sdl2-rk3588-install/include/SDL2/begin_code.h:
+/opt/sdl2-rk3588-install/include/SDL2/close_code.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_assert.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_atomic.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_audio.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_error.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_endian.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_mutex.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_thread.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_rwops.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_clipboard.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_cpuinfo.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_events.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_video.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_pixels.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_rect.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_surface.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_blendmode.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_keyboard.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_keycode.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_scancode.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_mouse.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_joystick.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_gamecontroller.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_sensor.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_quit.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_gesture.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_touch.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_filesystem.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_haptic.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_hints.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_loadso.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_log.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_messagebox.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_metal.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_power.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_render.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_shape.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_system.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_timer.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_version.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_locale.h:
+/opt/sdl2-rk3588-install/include/SDL2/SDL_misc.h:
+heimann_inter.h:
+heimann_drv.h:
+heimann_reg.h:
---
+++ Heimann_Cam/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+
+#include "heimann_drv.h"
+
+// extern int sensor_main(int argc,char *argv[]);
+
+int main(int argc, char *argv[]) {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        // 子进程：执行 sensor_main
+        printf("[Child] sensor_main starting...\n");
+        exit(sensor_main(argc, argv)); // 确保子进程退出时正确返回
+    } else {
+        // 父进程：可以执行别的逻辑，或者等待子进程
+        printf("[Parent] sensor_main forked, PID = %d\n", pid);
+
+        int status;
+        waitpid(pid, &status, 0);  // 等待子进程结束
+        if (WIFEXITED(status)) {
+            printf("[Parent] sensor_main exited with status %d\n", WEXITSTATUS(status));
+        } else {
+            printf("[Parent] sensor_main terminated abnormally.\n");
+        }
+    }
+
+    return 0;
+}
+
+// int main(int argc,char *argv[])
+// {
+//     int sensor_fd, eeprom_fd;
+
+//     // if(argc!=3){
+//     //     printf("Wrong use !!!!\n");
+//     //     printf("Usage: %s [sensor-i2c6] [eeprom-i2c5]\n",argv[0]);
+//     //     return -1; 
+//     // }
+
+//     // sensor_fd = open(argv[1], O_RDWR); // open file and enable read and  write
+//     // eeprom_fd = open(argv[2], O_RDWR);
+//     // if (sensor_fd < 0){
+//     //     printf("Can't open sensor_fd %s\n",argv[1]); // open i2c dev file fail
+//     //     exit(1);
+//     // } else {
+//     //     printf("sensor_fd: %s open successfully\n",argv[1]);
+//     // }
+//     // if (eeprom_fd < 0){
+//     //     printf("Can't open eeprom_fd %s \n",argv[2]); // open i2c dev file fail
+//     //     exit(1);
+//     // } else {
+//     //     printf("eeprom_fd: %s open successfully\n",argv[2]);
+//     // }
+
+//     sensor_init(sensor_fd, eeprom_fd);
+
+//     close(sensor_fd);
+//     close(eeprom_fd);
+
+//     return 0;
+
+// }
